bb78c569af909276941d50a2e12e061437b51399
Ahrar Shah <shahbakers786@gmail.com>
2025-10-07 19:34:27 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Allow sending cookies & sameSite none if needed — for demo same-origin is fine.
+}
+
+module.exports = nextConfig
